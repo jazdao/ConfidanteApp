46503281a1680dd0e369da5845c54ee9cf5720fb
Lance Dall <ldall2009@gmail.com>
2020-11-10 19:55:24 -0800

    Created Basic UI Template for Home screen and Profile Screen

--- app/src/main/java/com/bignerdranch/android/confidante/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/confidante/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.bignerdranch.android.confidante
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+//used in logging messages
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var profileButton: Button
+    private lateinit var findButton: Button
+    private lateinit var chatButton: Button
+    private lateinit var profilePicture: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        profileButton = findViewById(R.id.profile_button)
+        findButton = findViewById(R.id.find_button)
+        chatButton = findViewById(R.id.chat_button)
+        profilePicture = findViewById(R.id.profile_picture)
+
+        profilePicture.setImageResource(R.drawable.person);
+
+        profileButton.setOnClickListener {
+            val intent = ProfileActivity.newIntent(this@MainActivity)
+            Log.d(TAG, "Exited newIntent method in ProfileActivity")
+            startActivity(intent)
+        }
+        findButton.setOnClickListener {
+            Toast.makeText(this, R.string.find, Toast.LENGTH_SHORT).show()
+        }
+        chatButton.setOnClickListener {
+            Toast.makeText(this, R.string.chat, Toast.LENGTH_SHORT).show()
+        }
     }
 }
---
+++ app/src/main/java/com/bignerdranch/android/confidante/ProfileActivity.kt
@@ -0,0 +1,87 @@
+package com.bignerdranch.android.confidante
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
+import android.widget.ImageView
+
+//used in logging messages
+private const val TAG = "ProfileActivity"
+
+class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var profileBioField: EditText
+    private lateinit var profilePicture: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        profileBioField = findViewById(R.id.profile_bio)
+        profilePicture = findViewById(R.id.profile_picture)
+
+        profilePicture.setImageResource(R.drawable.person);
+
+        //listener for a bio change
+        val bioWatcher = object : TextWatcher {
+
+            override fun beforeTextChanged(
+                sequence: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+                // This space intentionally left blank
+            }
+
+            /**
+             * In onTextChanged(...), you call toString() on the CharSequence that is
+             * the user's input.  This function returns a string, which you then use
+             * to set the bio.
+             */
+            override fun onTextChanged(
+                sequence: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                // Eventually, we edit the bio here
+            }
+
+            override fun afterTextChanged(sequence: Editable?) {
+                // This one too
+            }
+        }
+
+        profileBioField.addTextChangedListener(bioWatcher)
+
+    }
+
+    private fun setAnswerShownResult() {
+        setResult(Activity.RESULT_OK)
+    }
+
+    /**
+     * A companion object allows you to access functions without having an instance of a class,
+     * similar to static functions in Java.  Using a newIntent(...) function inside a companion
+     * object like this for your activity subclasses will make it easy for other code to properly
+     * configure their launching intents.
+     */
+    companion object {
+        /**
+         * This function allows you to create an Intent properly configured with the extras
+         * CheatActivity will need.  The answerIsTrue argument, a Boolean, is put into the intent
+         * with a private name using the EXTRA_ANSWER_IS_TRUE constant.
+         */
+        fun newIntent(packageContext: Context): Intent {
+            Log.d(TAG, "Entered newIntent function in ProfileActivity")
+            return Intent(packageContext, ProfileActivity::class.java)
+        }
+    }
+}
