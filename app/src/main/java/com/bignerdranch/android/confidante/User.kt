08525394b892ed450116d251ce53cc74838e4b3d
tekkyboy <kparayao@gmail.com>
2020-11-28 18:42:12 -0800

    Updated User.Kt for chat purposes.

--- app/src/main/java/com/bignerdranch/android/confidante/User.kt
+++ app/src/main/java/com/bignerdranch/android/confidante/User.kt
@@ -1,22 +1,29 @@
 package com.bignerdranch.android.confidante
 
+import android.os.Parcelable
 import android.util.Log
+import kotlinx.android.parcel.Parcelize
 
-//used in logging messages
 private const val TAG = "User"
 
+@Parcelize
 class User(
+    var uid: String,
     var username: String,
     var email: String,
     var password: String,
-    var nameOfUser: String? = null,
+    var profileImageUrl: String,
     var userBio: String? = null,
-    var userListOfInterests: String? = null /**For now, this is just a String.  Later, this will be something like List<Interest>*/
-) {
+    var userListOfInterests: String? = null
+    /**For now, this is just a String.  Later, this will be something like List<Interest>*/
+): Parcelable {
+
+    constructor() : this("", "", "", "", "")
 
     init {
-        Log.d(TAG, "nameOfUser: $nameOfUser")
+        Log.d(TAG, "nameOfUser: $username")
         Log.d(TAG, "userBio: $userBio")
         Log.d(TAG, "userListOfInterests: $userListOfInterests")
+
     }
 }
