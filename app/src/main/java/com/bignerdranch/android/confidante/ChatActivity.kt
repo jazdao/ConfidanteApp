82e793983ee8c3686201daff98da081311f066db
tekkyboy <kparayao@gmail.com>
2020-11-28 18:40:17 -0800

    Files for the main chat page, chat logs, and a chat message class.

---
+++ app/src/main/java/com/bignerdranch/android/confidante/ChatActivity.kt
@@ -0,0 +1,74 @@
+package com.bignerdranch.android.confidante
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_chat.*
+import kotlinx.android.synthetic.main.user_message_row.view.*
+
+class ChatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+        supportActionBar?.title = "Select A Confidante"
+        fetchUsers()
+    }
+
+    companion object {
+        const val USER_KEY = "USER"
+    }
+
+    private fun fetchUsers(){
+
+        val ref = FirebaseDatabase.getInstance().getReference("Users")
+        ref.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+
+                val adapter = GroupAdapter<ViewHolder>()
+                p0.children.forEach{
+                    val user = it.getValue(User::class.java)
+                    if(user != null) {
+                        adapter.add(UserItem(user))
+                        }
+                }
+
+                adapter.setOnItemClickListener { item, view ->
+                    val userItem = item as UserItem
+                    val intent = Intent(view.context, ChatLogActivity::class.java)
+                    intent.putExtra(USER_KEY, userItem.user)
+                    startActivity(intent)
+                    finish()
+                }
+
+                recyclerview_chatlist.adapter = adapter
+            }
+
+            override fun onCancelled(error:  DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
+}
+
+class UserItem(val user: User): Item<ViewHolder>() {
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        viewHolder.itemView.username_textview_new_message.text = user.username
+        //Picasso.get().load(user.profileImageUrl).into(viewHolder.itemView.user_imageview_message)
+        //user.profileImageUrl to be implemented from the User class
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.user_message_row
+    }
+}
