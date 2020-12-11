6fb0510ee4baf14d8f56ee4e92b2849b95dce8d5
tekkyboy <kparayao@gmail.com>
2020-12-10 18:45:52 -0800

    Removed Action Bar

--- app/src/main/java/com/bignerdranch/android/confidante/LoginActivity.kt
+++ app/src/main/java/com/bignerdranch/android/confidante/LoginActivity.kt
@@ -24,6 +24,7 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_login)
 
         database = FirebaseDatabase.getInstance()
