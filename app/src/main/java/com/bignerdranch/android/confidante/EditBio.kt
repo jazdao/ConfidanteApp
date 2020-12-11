d32ede5be92e2a78b0107dab1d498fcf20d9cc30
tekkyboy <kparayao@gmail.com>
2020-12-11 09:51:06 -0800

    Updated EditBio

--- app/src/main/java/com/bignerdranch/android/confidante/EditBio.kt
+++ app/src/main/java/com/bignerdranch/android/confidante/EditBio.kt
@@ -1,26 +1,13 @@
 package com.bignerdranch.android.confidante
 
-import android.app.Activity
-import android.content.Context
-import android.content.Intent
-import android.graphics.drawable.BitmapDrawable
-import android.net.Uri
-import android.net.UrlQuerySanitizer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
+import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.ImageView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.storage.FirebaseStorage
-import kotlinx.android.synthetic.main.activity_profile.*
-import java.util.*
 
 //used in logging messages
 private const val TAG = "EditBio"
@@ -31,58 +18,31 @@ class EditBio : AppCompatActivity() {
     private lateinit var database: FirebaseDatabase
     private lateinit var reference: DatabaseReference
     private lateinit var editBioField: EditText
+    private lateinit var submitButton: Button
+    private lateinit var cancelButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.edit_bio)
 
+        val uid = FirebaseAuth.getInstance().uid
+
+        submitButton = findViewById(R.id.submit_button)
+        cancelButton = findViewById(R.id.cancel_button)
         editBioField = findViewById(R.id.edit_bio)
 
-        //listener for a bio change
-//        val bioWatcher = object : TextWatcher {
-//
-//            override fun beforeTextChanged(
-//                sequence: CharSequence?,
-//                start: Int,
-//                count: Int,
-//                after: Int
-//            ) {
-//                // This space intentionally left blank
-//            }
+        submitButton.setOnClickListener {
+            if (editBioField.text.toString().isEmpty()) {
+                Toast.makeText(this, "BioField not Changed", Toast.LENGTH_SHORT).show()
+            } else {
+                FirebaseDatabase.getInstance().reference.child("Users/$uid/UserBio").push().setValue(editBioField.text.toString())
+            }
+        }
 
-            /**
-             * In onTextChanged(...), you call toString() on the CharSequence that is
-             * the user's input.  This function returns a string, which you then use
-             * to set the bio.
-             */
-//            override fun onTextChanged(
-//                sequence: CharSequence?,
-//                start: Int,
-//                before: Int,
-//                count: Int
-//            ) {
-                // Eventually, we edit the bio here
-//                database = FirebaseDatabase.getInstance()
-//                reference = database.reference.child("Users").child("lmdall")
-//
-//                //hardcoded vars temporarily
-//                var name = "Lance Dall"
-//                var bio = "This is a default bio"
-//                var interestList = "This is a dummy placeholder sentence for what will end up being a list of interests"
-//
-//                val user = User(name, bio, interestList)
-//
-//                user.userBio = sequence.toString()
-//
-//                reference.setValue(user)
-//            }
-//
-//            override fun afterTextChanged(sequence: Editable?) {
-//                // This one too
-//            }
-//        }
-//
-//        editBioField.addTextChangedListener(bioWatcher)
+        cancelButton.setOnClickListener {
+            finish()
+        }
+
     }
 }
-
