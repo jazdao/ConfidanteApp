7ad70caedd4c5ccc818f9864189d7fd871255501
tekkyboy <kparayao@gmail.com>
2020-12-11 09:51:47 -0800

    Updated EditInterest

--- app/src/main/java/com/bignerdranch/android/confidante/EditInterest.kt
+++ app/src/main/java/com/bignerdranch/android/confidante/EditInterest.kt
@@ -1,42 +1,47 @@
 package com.bignerdranch.android.confidante
-import com.bignerdranch.android.confidante.InterestItem
-import com.bignerdranch.android.confidante.R
-import android.app.Activity
-import android.content.Context
-import android.content.Intent
-import android.graphics.drawable.BitmapDrawable
-import android.net.Uri
-import android.net.UrlQuerySanitizer
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
+import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.ImageView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.storage.FirebaseStorage
-import kotlinx.android.synthetic.main.activity_profile.*
-import java.util.*
 
 //used in logging messages
-private const val TAG = "EditBio"
+private const val TAG = "EditInterest"
 
-class EditBio : AppCompatActivity() {
+class EditInterest : AppCompatActivity() {
 
     private lateinit var profileBioField: EditText
     private lateinit var database: FirebaseDatabase
     private lateinit var reference: DatabaseReference
     private lateinit var editInterestItem: EditText
+    private lateinit var submitButton: Button
+    private lateinit var cancelButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.interest_row)
 
+        val uid = FirebaseAuth.getInstance().uid
+
+        submitButton = findViewById(R.id.submit_button)
+        cancelButton = findViewById(R.id.cancel_button)
         editInterestItem = findViewById(R.id.edit_interests)
+
+        submitButton.setOnClickListener {
+            if (editInterestItem.text.toString().isEmpty()) {
+                Toast.makeText(this, "InterestField not Changed", Toast.LENGTH_SHORT).show()
+            } else {
+                FirebaseDatabase.getInstance().reference.child("Users/$uid/UserInterest").push().setValue(editInterestItem.text.toString())
+            }
+        }
+
+        cancelButton.setOnClickListener {
+            finish()
+        }
     }
 }
